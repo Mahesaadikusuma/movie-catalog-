99ca9bdc57bcf1858abd5043d9b55bf3768e88b6
Mahesaadikusuma <mahesaadikuzuma@gmail.com>
2024-04-30 19:59:56 +0700

    first commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+/* eslint-disable quotes */
+const CONFIG = {
+  KEY: "8c001cc6ed68997e608b91bf16b520f0",
+  BASE_URL: "https://api.themoviedb.org/3/",
+  BASE_IMAGE_URL: "https://image.tmdb.org/t/p/w500/",
+  DEFAULT_LANGUAGE: "en-us",
+};
+
+export default CONFIG;
